241b913fed4183cbdea9a1ce9d2e840279982be7
Dell <robert.maksymow@tutanota.com>
2026-04-16 21:36:41 +0100

    Backend. 10 DNA simulator - create 30times

--- practice_backend/10_SimulatorDNA.js
+++ practice_backend/10_SimulatorDNA.js
@@ -14,52 +14,82 @@ const mockUpStrand = () => {
 };
 
 function pAequorFactory(number, dnaBases) {
-  //do something
-
+  const originalDNA = dnaBases;
+  // console.log("ORIGINAL DNA", originalDNA);
   return {
     specimenNum: number,
     dna: dnaBases,
     mutate() {
+      const oldDNA = this.dna;
       const generateRandomIndex = Math.floor(Math.random() * 15);
       let currentBase = this.dna[generateRandomIndex];
       const selectNewDNAbase = () => returnRandBase();
       let newDNAbase = selectNewDNAbase();
       while (currentBase === newDNAbase) {
         newDNAbase = selectNewDNAbase();
-        console.log("inside while", newDNAbase);
       }
       this.dna[generateRandomIndex] = newDNAbase;
-      console.log("this DNA after mutation", this.dna);
+      console.log("MUTATED DNA", this.dna);
     },
     compareDNA(obj) {
-      console.log("temp1", this.dna);
-      console.log("temp2", obj.dna);
       let counter = null;
       const calc = (num) => {
         return (num / this.dna.length) * 100;
       };
       for (i = 0; i < this.dna.length; i++) {
         for (j = 0; j < obj.dna.length; j++) {
-          console.log(i, this.dna[i], j, obj.dna[j]);
           if (i === j && this.dna[i] === obj.dna[j]) {
-            console.log("match");
             counter = counter + 1;
           }
         }
-        // console.log(this.dna[i])
       }
-      // console.log("counter", counter);
       const inCommon = Math.floor(calc(counter));
-      // console.log(inCommon);
       console.log(
         `specimen #${this.specimenNum} and specimen #${obj.specimenNum}  have ${inCommon}% DNA in common`,
       );
     },
+    willLikelySurvive() {
+      let counterC = null;
+      let counterG = null;
+      let calculateTotal = () =>
+        ((counterC + counterG) / this.dna.length) * 100;
+      for (i = 0; i < this.dna.length; i++) {
+        // console.log(i, this.dna[i]);
+        if (this.dna[i] === "C") {
+          counterC = counterC + 1;
+        } else if (this.dna[i] === "G") {
+          counterG = counterG + 1;
+        }
+      }
+      if (calculateTotal() > 60) {
+        console.log(counterC, counterG, calculateTotal());
+        return true;
+      } else {
+        return false;
+      }
+    },
   };
 }
 
-const temp1 = pAequorFactory(3, mockUpStrand());
-const temp2 = pAequorFactory(5, mockUpStrand());
+// const temp1 = pAequorFactory(3, mockUpStrand());
+// const temp2 = pAequorFactory(5, mockUpStrand());
 
+// CALL
 // temp1.mutate();
-temp1.compareDNA(temp2);
+// temp1.compareDNA(temp2);
+// CALL TO CREATE 30 instances of DNA
+const dna30times = [];
+const create30 = () => {
+  while (dna30times.length < 30) {
+    const newDNA = pAequorFactory(3, mockUpStrand());
+    const isTrue = newDNA.willLikelySurvive();
+    console.log("DNA", newDNA);
+    if (isTrue) {
+      dna30times.push(newDNA);
+    }
+  }
+  console.log("dna30 array", dna30times);
+  return dna30times;
+};
+
+create30();
