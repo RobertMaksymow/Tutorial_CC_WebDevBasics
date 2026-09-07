47bad5a167e441c278041250669c8e788415fb85
Dell <robert.maksymow@tutanota.com>
2026-09-07 17:55:46 +0100

    JS-Backend. Film finder

---
+++ practice_backend/random/randomCode.js
@@ -0,0 +1,8 @@
+function countVowels(str) {
+  //a, e, i, o, u
+  let count = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  const result = str.map((letter) => {
+    console.log(letter);
+  });
+}
